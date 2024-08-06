67888bf740f286b4ee1e07b1ab314b36ab8a3c8d
Gavriil Negoita <gavriil.negoita@mindera.com>
2024-08-06 22:34:51 +0300

    add base project structure

---
+++ src/test/java/com/inginterview/storemanagement/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.inginterview.storemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+}
