d49c0d7c8d31b82d4decacf5cf81a381b877ea8a
Gavriil Negoita <gavriil.negoita@mindera.com>
2024-08-08 12:13:48 +0300

    remove not null constraint on description fields

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,13 +4,13 @@ DROP TABLE IF EXISTS product_category;
 CREATE TABLE product_category (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
-    description VARCHAR(255) NOT NULL
+    description VARCHAR(255)
 );
 
 CREATE TABLE product (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
     price DECIMAL(10, 2) NOT NULL,
     stock BIGINT NOT NULL,
     category_id BIGINT NOT NULL,
