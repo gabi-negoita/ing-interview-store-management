c7faadf255dbe4c2546895bf5890cd331c82166c
Gavriil Negoita <gavriil.negoita@mindera.com>
2024-08-09 11:43:55 +0300

    add user repository and service logic

---
+++ src/main/java/com/inginterview/storemanagement/service/IsmUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.inginterview.storemanagement.service;
+
+import com.inginterview.storemanagement.exception.UserNotFoundException;
+import com.inginterview.storemanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class IsmUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        final var user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        final var authorities = user.getRoles().stream()
+                .flatMap(role -> role.getPermissions().stream())
+                .map(permission -> new SimpleGrantedAuthority(permission.getName()))
+                .collect(Collectors.toSet());
+
+        return new User(user.getUsername(), user.getPassword(), authorities);
+    }
+}
+
