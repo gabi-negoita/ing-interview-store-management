1f5909c16e706bd51509d972ab21264fe9a84c79
Gavriil Negoita <gavriil.negoita@mindera.com>
2024-08-09 20:13:06 +0300

    add integration tests for product rest controller

---
+++ src/test/java/com/inginterview/storemanagement/controller/AuthenticationIntegrationTest.java
@@ -0,0 +1,97 @@
+package com.inginterview.storemanagement.controller;
+
+import com.inginterview.storemanagement.entity.Permission;
+import com.inginterview.storemanagement.entity.Role;
+import com.inginterview.storemanagement.entity.User;
+import com.inginterview.storemanagement.repository.UserRepository;
+import com.inginterview.storemanagement.service.TokenAuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+import static org.mockito.Mockito.when;
+
+@ActiveProfiles("integration-tests")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AuthenticationIntegrationTest {
+
+    @Autowired
+    protected TestRestTemplate restTemplate;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Autowired
+    private TokenAuthenticationService tokenAuthenticationService;
+
+    private Set<Permission> getMockedPermissions() {
+        return Set.of(Permission.builder()
+                .id(1L)
+                .name("products")
+                .build());
+    }
+
+    protected String mockUserAndGetJwtToken(String username, String password, Set<String> permissions) {
+        mockUser(username, password, permissions);
+        return getJwtToken(username, password, permissions);
+    }
+
+    protected void mockUser(String username, String password, Set<String> permissions) {
+        final var mockedUser = getMockedUser(username, password, permissions);
+        when(userRepository.findByUsername(username)).thenReturn(Optional.of(mockedUser));
+    }
+
+    protected String getJwtToken(String username, String password, Set<String> permissions) {
+        return tokenAuthenticationService.generate(getAuthenticationWithPermissions(username, password, permissions));
+    }
+
+    private User getMockedUser(String username, String password, Set<String> permissions) {
+        return User.builder()
+                .id(1L)
+                .username(username)
+                .password(getEncodedPassword(password))
+                .enabled(true)
+                .roles(Set.of(getMockedRole(permissions)))
+                .build();
+    }
+
+    private String getEncodedPassword(String password) {
+        return new BCryptPasswordEncoder(10).encode(password);
+    }
+
+    private Role getMockedRole(Set<String> permissions) {
+        return Role.builder()
+                .id(1L)
+                .name("ROLE_USER")
+                .permissions(getMockedPermissions(permissions))
+                .build();
+    }
+
+    private Set<Permission> getMockedPermissions(Set<String> permissions) {
+        final var idCounter = new AtomicLong(1);
+        return permissions.stream()
+                .map(permission -> Permission.builder()
+                        .id(idCounter.getAndIncrement())
+                        .name(permission)
+                        .build())
+                .collect(Collectors.toSet());
+    }
+
+    private Authentication getAuthenticationWithPermissions(String username, String password, Set<String> permissions) {
+        return new UsernamePasswordAuthenticationToken(
+                username,
+                password,
+                permissions.stream().map(SimpleGrantedAuthority::new).toList());
+    }
+}
---
+++ src/test/java/com/inginterview/storemanagement/controller/ProductRestControllerTest.java
@@ -0,0 +1,67 @@
+package com.inginterview.storemanagement.controller;
+
+import com.inginterview.storemanagement.entity.Product;
+import com.inginterview.storemanagement.model.ErrorResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class ProductRestControllerTest extends AuthenticationIntegrationTest {
+
+    @Test
+    void whenGetAllProducts_ifAuthenticated_thenReturnsListOfProducts() {
+        final var jwtToken = mockUserAndGetJwtToken("user", "pass", Set.of("products"));
+
+        final var headers = new HttpHeaders();
+        headers.setBearerAuth(jwtToken);
+
+        final var entity = new HttpEntity<>(headers);
+
+        final var response = restTemplate.exchange("/products",
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<List<Product>>() {
+                });
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().size()).isGreaterThan(0);
+    }
+
+    @NullSource
+    @ValueSource(strings = {"Bearer", "Bearer    ", "Bearer invalid-token"})
+    @ParameterizedTest
+    void whenGetAllProducts_ifTokenAbsentOrInvalid_thenReturn401Unauthorized(String token) {
+        mockUser("user", "pass", Set.of("products"));
+
+        final var headers = new HttpHeaders();
+        if (token != null) {
+            headers.set("Authorization", token);
+        }
+        final var entity = new HttpEntity<>(headers);
+
+        final var response = restTemplate.exchange("/products",
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<ErrorResponse>() {
+                });
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getStatus()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+        assertThat(response.getBody().getTimestamp()).isNotNull();
+        assertThat(response.getBody().getErrors().size()).isEqualTo(1);
+        assertThat(response.getBody().getErrors().get(0)).isEqualTo("Full authentication is required to access this resource");
+    }
+}
