6c97ba80dfb231f3777f9c56a9fcdbeca0902db2
Gavriil Negoita <gavriil.negoita@mindera.com>
2024-08-09 20:11:57 +0300

    rename AuthController to AuthRestController

--- src/main/java/com/inginterview/storemanagement/controller/AuthRestController.java
+++ src/main/java/com/inginterview/storemanagement/controller/AuthRestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/auth")
-public class AuthController {
+public class AuthRestController {
 
     private final AuthenticationService authenticationService;
 
