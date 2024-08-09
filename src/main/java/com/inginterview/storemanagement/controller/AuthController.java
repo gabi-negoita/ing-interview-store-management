f020789fa6c7511d135034ee580eedcaecd24a80
Gavriil Negoita <gavriil.negoita@mindera.com>
2024-08-09 13:38:45 +0300

    move AuthController logic to dedicated service

--- src/main/java/com/inginterview/storemanagement/controller/AuthController.java
+++ src/main/java/com/inginterview/storemanagement/controller/AuthController.java
@@ -2,12 +2,9 @@ package com.inginterview.storemanagement.controller;
 
 import com.inginterview.storemanagement.model.JwtAuthenticationResponse;
 import com.inginterview.storemanagement.model.LoginRequest;
-import com.inginterview.storemanagement.service.TokenAuthenticationService;
+import com.inginterview.storemanagement.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,23 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/auth")
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final TokenAuthenticationService tokenProvider;
+    private final AuthenticationService authenticationService;
 
     @PostMapping(value = "/token")
     @ResponseStatus(HttpStatus.OK)
     public JwtAuthenticationResponse authenticate(@RequestBody LoginRequest loginRequest) {
-        final var authToken = new UsernamePasswordAuthenticationToken(
-                loginRequest.getUsername(),
-                loginRequest.getPassword());
-        final var authentication = authenticationManager.authenticate(authToken);
-        final var jwt = tokenProvider.generate(authentication);
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        return JwtAuthenticationResponse.builder()
-                .tokenType("Bearer")
-                .accessToken(jwt)
-                .build();
+        return authenticationService.authenticateAndGenerateToken(loginRequest.getUsername(), loginRequest.getPassword());
     }
 }
---
+++ src/main/java/com/inginterview/storemanagement/service/AuthenticationService.java
@@ -0,0 +1,29 @@
+package com.inginterview.storemanagement.service;
+
+import com.inginterview.storemanagement.model.JwtAuthenticationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final TokenAuthenticationService tokenProvider;
+
+    public JwtAuthenticationResponse authenticateAndGenerateToken(String username, String password) {
+        final var authToken = new UsernamePasswordAuthenticationToken(username, password);
+        final var authentication = authenticationManager.authenticate(authToken);
+        final var jwt = tokenProvider.generate(authentication);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return JwtAuthenticationResponse.builder()
+                .tokenType("Bearer")
+                .accessToken(jwt)
+                .build();
+    }
+}
